458c6768e944b5c25e19e09ad58d97df49bc9186
hrtCoding <86431981+hrtCoding@users.noreply.github.com>
2024-05-19 22:57:15 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+
+menuBtn.addEventListener("click", (e) => {
+navLinks.classList.toggle("open");
+
+});
+
